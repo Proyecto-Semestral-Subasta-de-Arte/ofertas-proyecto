8c2389e744a3f28f5e164be6a2049d38481ecc98
Gonza-2103 <ghormazabal.d@gmail.com>
2026-06-11 21:22:03 -0400

    Anotación @Schema en 'request' y 'response' DTO

--- src/main/java/cl/sda1085/ofertas/dto/OfertaRequestDTO.java
+++ src/main/java/cl/sda1085/ofertas/dto/OfertaRequestDTO.java
@@ -1,5 +1,6 @@
 package cl.sda1085.ofertas.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -11,15 +12,20 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Modelo requerido (JSON) para registrar o actualizar una puja/oferta en el sistema.")
+
 public class OfertaRequestDTO {
 
+    @Schema(description = "Monto económico de la puja que se desea postular.", example = "175000.00", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull(message = "El monto es obligatorio.")
     @Positive(message = "El monto debe ser mayor a cero.")
     private BigDecimal monto;
 
+    @Schema(description = "Identificador único del usuario comprador proveniente del microservicio 'usuarios'.", example = "6", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull(message = "El ID del usuario es obligatorio.")
     private Long idUsuario;
 
+    @Schema(description = "Identificador único de la subasta activa proveniente del microservicio 'subastas'.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull(message = "El ID de la subasta es obligatorio.")
     private Long idSubasta;
 }
--- src/main/java/cl/sda1085/ofertas/dto/OfertaResponseDTO.java
+++ src/main/java/cl/sda1085/ofertas/dto/OfertaResponseDTO.java
@@ -1,5 +1,6 @@
 package cl.sda1085.ofertas.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.hateoas.RepresentationModel;
 
@@ -12,15 +13,24 @@ import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = false)
 @Builder
-
+@Schema(description = "Estructura de respuesta que retorna el servidor con los detalles consolidados de la puja junto a los enlaces HATEOAS.")
 public class OfertaResponseDTO extends RepresentationModel<OfertaResponseDTO> {
 
     //DTO de salida (respuesta).
     //No existen las anotaciones de validación.
 
+    @Schema(description = "Identificador único incremental autogenerado en la base de datos de ofertas.", example = "2")
     private Long id;
+
+    @Schema(description = "Monto final validado y almacenado para la puja.", example = "175000.00")
     private BigDecimal monto;
+
+    @Schema(description = "Fecha y hora exacta en la que se interceptó e ingresó la oferta al sistema.", example = "2026-06-11T21:15:30")
     private LocalDateTime fechaHora;
+
+    @Schema(description = "ID del usuario comprador que realizó la oferta.", example = "6")
     private Long idUsuario;
+
+    @Schema(description = "ID de la subasta en la cual se compitió.", example = "1")
     private Long idSubasta;
 }
